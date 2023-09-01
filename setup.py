15bcd72c2ce16017f611da91a6b768d49953bef1
John San Soucie <jsansoucie@whoi.edu>
2023-09-01 18:57:29 +0000

    changed python packages into rosdep-compatible dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=["fake_ifcb"], package_dir={"": "src"}, install_requires=["pandas"]
+    packages=["fake_ifcb"], package_dir={"": "src"}, install_requires=["pandas", "opencv-python"]
 )
 
 setup(**setup_args)
